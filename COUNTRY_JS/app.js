353a71570d03235bfc7ab02fb8cdc5af0cfa7a70
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-12-12 13:46:54 +0000

    03.2.1

--- COUNTRY_JS/app.js
+++ COUNTRY_JS/app.js
@@ -24,6 +24,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/countries', countryRouter);
 
+app.use(function(err, req, res, next) {
+ // set locals, only providing error in development
+ res.locals.message = err.message;
+ res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+ // render the error page
+ res.status(err.status || 500);
+ res.render('error');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- COUNTRY_JS/routes/index.js
+++ COUNTRY_JS/routes/index.js
@@ -1,9 +1,20 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
+var country = require('countryjs');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', { title: 'Express' });
 });
+
+router.get('/pages/:state/', function(req, res, next){
+ if (typeof country.info(req.params.state) === "undefined") {
+   return next(createError(422, 'OOPS! State not found'));
+ }
+ else
+ {
+   res.render('state', {state: country.info(req.params.state)}) ; //Rendiamo lo stato un parametro
+ }
+})
 
 module.exports = router;
