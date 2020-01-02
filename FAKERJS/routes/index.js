1443b6fac1aa4d0fac868b675d612bf7410f8c3d
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2020-01-02 22:09:17 +0000

    punto 3 - fakerjs

--- FAKERJS/routes/index.js
+++ FAKERJS/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const peopleJson = require('../people.json');
+var faker = require('faker');
+var createError = require('http-errors');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +10,31 @@ router.get('/', function(req, res, next) {
         title: 'Home',
         people: peopleJson.people
     });
+});
+
+router.get('/scrittori/:person/', function(req, res, next) {
+    const person = peopleJson.people.find(p => p.name === req.params.person);
+
+    let quotes = [];
+    i = 0;
+
+    while(i < 10){
+        let sentence = faker.lorem.sentence();
+        quotes.push(sentence);
+        i++;
+    }
+
+    res.render('scrittori', {
+        person,
+        quotes
+    })
+});
+
+router.get('/dettagli/:person/', function(req, res, next) {
+    const person = peopleJson.people.find(p => p.name === req.params.person);
+    res.render('dettagli', {
+        person
+    })
 });
 
 module.exports = router;
